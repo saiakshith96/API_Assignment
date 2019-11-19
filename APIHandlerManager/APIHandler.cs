e60614b4051809a21bb60f929a3bd215d9426960
Sai Akshith Thodupunuri <tsaiakshith@gmail.com>
2019-11-19 12:05:02 -0500

    push1

--- APIHandlerManager/APIHandler.cs
+++ APIHandlerManager/APIHandler.cs
@@ -60,9 +60,48 @@ namespace API_Assignment.APIHandlerManager
                 {
                     // JsonConvert is part of the NewtonSoft.Json Nuget package
                     parks = JsonConvert.DeserializeObject<Mymodel>(parksData);
-                    
+
                 }
             }
+
+
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return parks;
+        }
+
+        public Mymodel GetModel1()
+        {
+            string NATIONAL_PARK_API_PATH = BASE_URL + "/323121?";
+            string parksData = "";
+
+            Mymodel parks = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+
+            // It can take a few requests to get back a prompt response, if the API has not received
+            //  calls in the recent past and the server has put the service on hibernation
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+                if (response.IsSuccessStatusCode)
+                {
+                    parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!parksData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    parks = JsonConvert.DeserializeObject<Mymodel>(parksData);
+
+                }
+            }
+
+
             catch (Exception e)
             {
                 // This is a useful place to insert a breakpoint and observe the error message
